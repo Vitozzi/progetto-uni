428efda805dc838df22b20eab632b894a4ace6ea
Vito <costantinov10@gmail.com>
2026-03-05 19:07:23 +0100

    prima bozza random

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { 
+  IonHeader, IonToolbar, IonTitle, IonContent, 
+  IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent,
+  IonList, IonItem, IonLabel, IonIcon 
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { peopleOutline, calendarOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent, 
+    IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent,
+    IonList, IonItem, IonLabel, IonIcon
+  ],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    // Questo serve per far apparire le icone!
+    addIcons({ peopleOutline, calendarOutline });
+  }
+
+  funzioneDiEsempio() {
+    console.log("THE GOD ha cliccato il tasto!");
+    alert("Hai cliccato la Gestione Studenti!");
+  }
 }
